8d387c770c61251bf70a3c3faa76613d571079d4
Emmanuel Valdez <evaldez89@gmail.com>
2019-05-12 15:38:18 -0400

    add home component

--- 02-angularbases/src/app/app.module.ts
+++ 02-angularbases/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
