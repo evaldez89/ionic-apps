d4abe67a1258ac90fa8f5b8a119d336847d2d835
Emmanuel Valdez <evaldez89@gmail.com>
2019-05-12 15:30:41 -0400

    Add pages/contact and about components

--- 02-angularbases/src/app/app.module.ts
+++ 02-angularbases/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
