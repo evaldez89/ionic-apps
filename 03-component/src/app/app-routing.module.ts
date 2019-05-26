f0713174e7715616957b7cff8ff1a3eecd069f7a
Emmanuel Valdez <evaldez89@gmail.com>
2019-05-26 15:59:20 -0400

    section 4 - add page action-sheet

--- 03-component/src/app/app-routing.module.ts
+++ 03-component/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' },
   { path: 'alert', loadChildren: './pages/alert/alert.module#AlertPageModule' },
+  { path: 'action-sheet', loadChildren: './pages/action-sheet/action-sheet.module#ActionSheetPageModule' },
 ];
 
 @NgModule({
