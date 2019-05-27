98c5d5e1140ebded5ba1e66e1f6a844d30886b8c
Emmanuel Valdez <evaldez89@gmail.com>
2019-05-26 23:09:26 -0400

    section 4 - Use ion-list to create a list of desire routes to go to (replacing the buttons)

--- 03-component/src/app/pages/inicio/inicio.page.ts
+++ 03-component/src/app/pages/inicio/inicio.page.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  childComponents: ChildComponent[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface ChildComponent {
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
