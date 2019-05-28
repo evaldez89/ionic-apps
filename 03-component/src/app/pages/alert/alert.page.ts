dcc8c201655a05bec1ea33d987611a0b376c0b7f
Emmanuel Valdez <evaldez89@gmail.com>
2019-05-28 07:30:27 -0400

    section 5 - Add alert controller with 2 button to the alert page

--- 03-component/src/app/pages/alert/alert.page.ts
+++ 03-component/src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -9,9 +10,34 @@ export class AlertPage implements OnInit {
 
   title = 'Alert page';
 
-  constructor() { }
+  constructor( public alertCtrl: AlertController ) { }
 
   ngOnInit() {
   }
 
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Boton Cancelar');
+          }
+        },
+        {
+          text: 'Ok',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Boton OK');
+          }
+        },
+      ]
+    });
+    return await alert.present();
+  }
 }
