114c4bb9a1b3b75394ead6b290e6106703702196
Emmanuel Valdez <evaldez89@gmail.com>
2019-06-05 21:03:50 -0400

    section 5 - Add checkboxes page

---
+++ 03-component/src/app/pages/check/check.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check',
+  templateUrl: './check.page.html',
+  styleUrls: ['./check.page.scss'],
+})
+export class CheckPage implements OnInit {
+
+  title = 'Checkbox';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
