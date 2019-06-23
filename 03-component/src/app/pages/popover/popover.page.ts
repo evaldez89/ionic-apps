f32cadd5f4ebdb0fd887927f2d4096e3dcbab5c6
Emmanuel Valdez <evaldez89@gmail.com>
2019-06-22 20:32:22 -0400

    section 5 - Add method to show popover on each button click

--- 03-component/src/app/pages/popover/popover.page.ts
+++ 03-component/src/app/pages/popover/popover.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopinfoComponent } from 'src/app/components/popinfo/popinfo.component';
 
 @Component({
   selector: 'app-popover',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopoverPage implements OnInit {
 
-  constructor() { }
+  constructor( private popOverCtrl: PopoverController ) { }
 
   ngOnInit() {
+  }
+
+  async showPop() {
+    const popover = await this.popOverCtrl.create({
+      component: PopinfoComponent,
+    });
+
+    await popover.present();
   }
 
 }
