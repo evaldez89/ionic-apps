d5d11ddb2a9dd224478231e4ce72bc63dcb0077e
Emmanuel Valdez <evaldez89@gmail.com>
2019-07-10 13:05:17 -0400

    section 7 - print api restult to console

--- 04-noticias/src/app/services/noticias.service.ts
+++ 04-noticias/src/app/services/noticias.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiasService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getToHeadLines() {
+    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=5c0cb46560344e3db2e61c647c977357`);
+  }
 }
