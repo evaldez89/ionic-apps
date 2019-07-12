95b014a4e7ebe49fdf8c42f209480aa53558275b
Emmanuel Valdez <evaldez89@gmail.com>
2019-07-11 21:07:51 -0400

    section 7 - prepare tab1 html to show the news content

--- 04-noticias/src/app/services/noticias.service.ts
+++ 04-noticias/src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TopHeadLinesResponse } from '../interfaces/news.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class NoticiasService {
   constructor( private http: HttpClient ) { }
 
   getToHeadLines() {
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=5c0cb46560344e3db2e61c647c977357`);
+    return this.http.get<TopHeadLinesResponse>(`https://newsapi.org/v2/top-headlines?country=us&apiKey=5c0cb46560344e3db2e61c647c977357`);
   }
 }
