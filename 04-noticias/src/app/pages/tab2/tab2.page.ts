c1c6e5f3853bf327fc413c8a50827b068e4df3f3
Emmanuel Valdez <evaldez89@gmail.com>
2019-07-12 19:29:20 -0400

    section 7 - make segment default value to be the first of the categories

--- 04-noticias/src/app/pages/tab2/tab2.page.ts
+++ 04-noticias/src/app/pages/tab2/tab2.page.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  categories = ['sports', 'business', 'entertainment', 'general', 'health', 'science', 'technology'];
+
+  @ViewChild(IonSegment) categoriesSegment: IonSegment;
 
   constructor() {}
+
+  ngOnInit() {
+    this.categoriesSegment.value = this.categories[0];
+  }
 
 }
