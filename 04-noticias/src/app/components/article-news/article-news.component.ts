0fa5f4465bd00626a596db3e667a33f8499102b9
Emmanuel Valdez <evaldez89@gmail.com>
2019-07-14 15:09:59 -0400

    section 7 - open clicked news url in the device default browser

--- 04-noticias/src/app/components/article-news/article-news.component.ts
+++ 04-noticias/src/app/components/article-news/article-news.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/interfaces/news.interface';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-article-news',
@@ -11,8 +12,12 @@ export class ArticleNewsComponent implements OnInit {
   @Input() newsArticle: Article;
   @Input() newsIndex: number;
 
-  constructor() { }
+  constructor( private iab: InAppBrowser ) { }
 
   ngOnInit() {}
+
+  openNews() {
+    const browser = this.iab.create(this.newsArticle.url, '_system' );
+  }
 
 }
