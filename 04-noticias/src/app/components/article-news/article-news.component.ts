61803b1eacd29c86f3741dfc23df2472b157b3b3
Emmanuel Valdez <evaldez89@gmail.com>
2019-07-14 17:29:27 -0400

    section 7 - add action button with options to share and save in favorite

--- 04-noticias/src/app/components/article-news/article-news.component.ts
+++ 04-noticias/src/app/components/article-news/article-news.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/interfaces/news.interface';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-article-news',
@@ -12,12 +13,44 @@ export class ArticleNewsComponent implements OnInit {
   @Input() newsArticle: Article;
   @Input() newsIndex: number;
 
-  constructor( private iab: InAppBrowser ) { }
+  constructor( private iab: InAppBrowser,
+               private actionSheetCtrl: ActionSheetController ) { }
 
   ngOnInit() {}
 
   openNews() {
     const browser = this.iab.create(this.newsArticle.url, '_system' );
+  }
+
+  async openShareMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Share',
+          icon: 'share',
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: 'heart',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
   }
 
 }
