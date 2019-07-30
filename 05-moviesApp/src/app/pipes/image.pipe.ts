830939eeb00a1220dcecbc9a1e6621939d5cd370
Emmanuel Valdez <evaldez89@gmail.com>
2019-07-29 23:49:07 -0400

    section 7 - add pipe logic to return image url

--- 05-moviesApp/src/app/pipes/image.pipe.ts
+++ 05-moviesApp/src/app/pipes/image.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const URL_IMAGE = 'https://image.tmdb.org/t/p/';
+
 @Pipe({
   name: 'image'
 })
 export class ImagePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(img: string, size: string = 'w500'): string {
+    if (!img) {
+      return;
+    }
 
+    return `${ URL_IMAGE }${ size }/${ img }`;
+  }
 }
