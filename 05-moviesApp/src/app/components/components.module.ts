bf1be805771541e9602a0b0cb4dd80c6997f0fd7
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-06 12:53:10 -0400

    section 7 - add custom slides components to components module declarations and imports to be able to use it in others components

--- 05-moviesApp/src/app/components/components.module.ts
+++ 05-moviesApp/src/app/components/components.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PipesModule } from '../pipes/pipes.module';
+import { IonicModule } from '@ionic/angular';
+import { SlideshowBackdropComponent } from './slideshow-backdrop/slideshow-backdrop.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SlideshowBackdropComponent
+  ],
+  exports: [
+    SlideshowBackdropComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    PipesModule,
+    IonicModule
   ]
 })
 export class ComponentsModule { }
