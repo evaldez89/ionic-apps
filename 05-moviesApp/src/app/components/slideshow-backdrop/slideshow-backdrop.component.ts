8c9e43c6cf7d83eb431079b1c4e6290cd9a1ed5b
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-06 08:03:35 -0400

    section 7 - add slideshows component

---
+++ 05-moviesApp/src/app/components/slideshow-backdrop/slideshow-backdrop.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow-backdrop',
+  templateUrl: './slideshow-backdrop.component.html',
+  styleUrls: ['./slideshow-backdrop.component.scss'],
+})
+export class SlideshowBackdropComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
