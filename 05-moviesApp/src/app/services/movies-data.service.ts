5d123398748d8a7018610b459c54fa2669924416
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-05 22:28:53 -0400

    section 7 - refactor method get movies to use global variables and improve readability

--- 05-moviesApp/src/app/services/movies-data.service.ts
+++ 05-moviesApp/src/app/services/movies-data.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { ResponseHeader } from '../interfaces/movie.interface';
 import { environment } from 'src/environments/environment';
 
+const BASE_URL = environment.url;
+const API_KEY = environment.apiKey;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,8 +13,15 @@ export class MoviesDataService {
 
   constructor( private http: HttpClient ) { }
 
+  private contructApiQuery<T>( query: string, lang = 'es' ) {
+    query = BASE_URL + query;
+    query += `&api_key=${ API_KEY }&language=${ lang }&include_image_language=${ lang }`;
+
+    return this.http.get<T>(query);
+  }
+
   getMovies() {
-    // tslint:disable-next-line:max-line-length
-    return this.http.get<ResponseHeader>(`${environment.url}/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-01-31&api_key=${environment.apiKey}&language=es&include_image_language=es`);
+    const query = '/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-01-31';
+    return this.contructApiQuery<ResponseHeader>(query);
   }
 }
