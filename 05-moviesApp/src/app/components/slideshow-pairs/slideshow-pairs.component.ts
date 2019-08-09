fd4d509e67e9f15649222cd1f9f9be0fa4a6c02b
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-09 12:41:43 -0400

    section 7 - add custom component to show poster in two rows

---
+++ 05-moviesApp/src/app/components/slideshow-pairs/slideshow-pairs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from 'src/app/interfaces/movie.interface';
+
+@Component({
+  selector: 'app-slideshow-pairs',
+  templateUrl: './slideshow-pairs.component.html',
+  styleUrls: ['./slideshow-pairs.component.scss'],
+})
+export class SlideshowPairsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
