1e3fcf20cf5756ea30e25efd157951f5ded44976
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-17 11:55:33 -0400

    section 7 - show modal with the details of a movie when is clicked in the pairs component

--- 05-moviesApp/src/app/components/slideshow-pairs/slideshow-pairs.component.ts
+++ 05-moviesApp/src/app/components/slideshow-pairs/slideshow-pairs.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Movie } from 'src/app/interfaces/movie.interface';
+import { MovieDetailComponent } from '../movie-detail/movie-detail.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-slideshow-pairs',
@@ -16,12 +18,25 @@ export class SlideshowPairsComponent implements OnInit {
     freeMode: true
   };
 
-  constructor() { }
+  constructor( public modalController: ModalController ) { }
 
   ngOnInit() {}
 
   nextMovies() {
     this.loadNextPage.emit();
+  }
+
+  async showMovieDetail( movie: Movie ) {
+    const movieModal = await this.modalController.create({
+      component: MovieDetailComponent,
+      componentProps: {
+        movie
+      }
+    });
+
+    await movieModal.present();
+
+    return movieModal;
   }
 
 }
