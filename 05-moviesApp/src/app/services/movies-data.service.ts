1953876cf2962d495a572bbf4abd2d417df0ce78
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-18 14:38:43 -0400

    section 7 - add services and interfaces for movie details and credits

--- 05-moviesApp/src/app/services/movies-data.service.ts
+++ 05-moviesApp/src/app/services/movies-data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ResponseHeader } from '../interfaces/movie.interface';
 import { environment } from 'src/environments/environment';
+import { MovieDetails } from '../interfaces/movie.details.interface';
+import { MovieCredits } from '../interfaces/movie.credits.interface';
 
 const BASE_URL = environment.url;
 const API_KEY = environment.apiKey;
@@ -31,5 +33,13 @@ export class MoviesDataService {
     const query = `/discover/movie?sort_by=popularity.desc&page=${ ++this.popularMoviesPage }`;
 
     return this.constructApiQuery<ResponseHeader>(query, lang);
+  }
+
+  getMovieDetails( movieId: number, lang = 'es' ) {
+    return this.constructApiQuery<MovieDetails>(`/movie/${ movieId }?a=1`, lang);
+  }
+
+  getMovieCredits( movieId: number, lang = 'es' ) {
+    return this.constructApiQuery<MovieCredits>(`/movie/${ movieId }/credits?a=1`, lang);
   }
 }
