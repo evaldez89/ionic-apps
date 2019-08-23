cc8ac062d8f478af3ffb179aa190d18907e0486c
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-23 08:25:56 -0400

    section 7 - present toast when movie is added to or removed from favorites

--- 05-moviesApp/src/app/services/local-storage.service.ts
+++ 05-moviesApp/src/app/services/local-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Movie } from '../interfaces/movie.interface';
 import { MovieDetails } from '../interfaces/movie.details.interface';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +11,29 @@ export class LocalStorageService {
 
   favoriteMovies: MovieDetails[] = [];
 
-  constructor( private storage: Storage ) { }
+  constructor( private storage: Storage,
+               private toastController: ToastController ) { }
+
+  async presentToast( message ) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 1500
+    });
+    toast.present();
+  }
 
   saveFavorite( movie: MovieDetails ) {
     const favoriteMovie = this.favoriteMovies.find( fav => movie.id === fav.id );
+    let message = 'Movie removed from favorites';
 
     if (favoriteMovie) {
       this.favoriteMovies = this.favoriteMovies.filter( fav => fav.id !== favoriteMovie.id );
     } else {
       this.favoriteMovies.push( movie );
+      message = 'Movie added to favorites';
     }
 
     this.storage.set('favoriteMovies', this.favoriteMovies);
+    this.presentToast(message);
   }
 }
