0f43dcda929c49a5d7ec9b76fbec504b96b8d2ca
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-23 07:58:28 -0400

    section 7 - method to save favorite movies in local storage

--- 05-moviesApp/src/app/services/local-storage.service.ts
+++ 05-moviesApp/src/app/services/local-storage.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Movie } from '../interfaces/movie.interface';
+import { MovieDetails } from '../interfaces/movie.details.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService {
 
-  constructor() { }
+  favoriteMovies: MovieDetails[] = [];
+
+  constructor( private storage: Storage ) { }
+
+  saveFavorite( movie: MovieDetails ) {
+    this.favoriteMovies.push( movie );
+    this.storage.set('favoriteMovies', this.favoriteMovies);
+  }
 }
