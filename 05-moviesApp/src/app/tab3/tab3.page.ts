ff9b72d6e2ad11bc76c9d0526a31736fb246a660
Emmanuel Valdez <evaldez89@gmail.com>
2019-10-04 16:26:44 -0400

    section 9 - show favorite movies on tab 3

--- 05-moviesApp/src/app/tab3/tab3.page.ts
+++ 05-moviesApp/src/app/tab3/tab3.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieDetails } from '../interfaces/movie.details.interface';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  favoriteMovies: MovieDetails[] = [];
+
+  constructor( private localData: LocalStorageService ) {}
+
+  async ngOnInit() {
+    this.favoriteMovies = await this.localData.loadFavorites();
+  }
 
 }
