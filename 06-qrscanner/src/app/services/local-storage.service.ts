6a72530bf8571c8c626568348be50f6b6dbd3751
Emmanuel Valdez <evaldez89@gmail.com>
2019-10-10 23:24:30 -0400

    section 10 - make data persintent in local storage

--- 06-qrscanner/src/app/services/local-storage.service.ts
+++ 06-qrscanner/src/app/services/local-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { LogScans } from '../models/log.model';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,19 @@ export class LocalStorageService {
 
   savedLogScans: LogScans[] = [];
 
-  constructor() { }
+  constructor( private storage: Storage ) {
+    this.storage.get('scanLogs').then( res => {
+      this.savedLogScans = (res) ? res : [];
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 
   saveLogScan( format: string, text: string ) {
     const newScan = new LogScans(format, text);
 
     this.savedLogScans.unshift( newScan );
+
+    this.storage.set('scanLogs', this.savedLogScans);
   }
 }
