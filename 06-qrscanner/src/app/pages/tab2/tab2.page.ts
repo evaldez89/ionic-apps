f9afea581f0de2f8b221360601c092670d235c0f
Emmanuel Valdez <evaldez89@gmail.com>
2019-10-10 23:03:21 -0400

    section 10 - show scanned logs

--- 06-qrscanner/src/app/pages/tab2/tab2.page.ts
+++ 06-qrscanner/src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { LogScans } from 'src/app/models/log.model';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor( public localStorage: LocalStorageService ) { }
+
+  sendByEmail() {
+
+  }
+
+  openScannedData( logScan: LogScans ) {
+
+  }
 
 }
