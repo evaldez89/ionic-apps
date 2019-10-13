5466ca1bdb439ddc4f919f4fe32839382ecf4bfa
Emmanuel Valdez <evaldez89@gmail.com>
2019-10-12 22:55:26 -0400

    section 10 - add InAppBrowser plugin

--- 06-qrscanner/src/app/app.module.ts
+++ 06-qrscanner/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
 
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { IonicStorageModule } from '@ionic/storage';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { IonicStorageModule } from '@ionic/storage';
     StatusBar,
     SplashScreen,
     BarcodeScanner,
+    InAppBrowser,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
