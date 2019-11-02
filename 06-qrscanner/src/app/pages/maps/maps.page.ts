aba5fb2c6f4cc5bc184eec72dd3062ca5531f070
Emmanuel Valdez <evaldez89@gmail.com>
2019-11-01 20:12:27 -0400

    show map with navigation control on the map view

--- 06-qrscanner/src/app/pages/maps/maps.page.ts
+++ 06-qrscanner/src/app/pages/maps/maps.page.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+// Since the CDN was imported in the index.html, we can assure this variable exist
+// and this way we can make sure the corrector does not complain about it
+declare var mapboxgl: any;
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.page.html',
   styleUrls: ['./maps.page.scss'],
 })
-export class MapsPage implements OnInit {
+export class MapsPage implements OnInit, AfterViewInit {
 
   latitude: number;
   longitud: number;
@@ -19,6 +23,17 @@ export class MapsPage implements OnInit {
     const args = geo.split(',');
     this.latitude = Number(args[0]);
     this.longitud = Number(args[1]);
+  }
+
+  ngAfterViewInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZXZhbGRlejg5IiwiYSI6ImNrMXBmeGs2MTA4NWQzbW82MzZkYmgxOTQifQ.V4eMf-oP_P88WfPZ2FblaQ';
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
   }
 
 }
