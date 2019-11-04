b8bf7870a7e4579ca2d67f1baa78c3105dd70f65
Emmanuel Valdez <evaldez89@gmail.com>
2019-11-03 23:09:34 -0400

    create initial configuration method for onesignal

--- 07-pushapp/src/app/services/push.service.ts
+++ 07-pushapp/src/app/services/push.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
 
-  constructor() { }
+  constructor( private oneSignal: OneSignal ) { }
+
+  initialConfig() {
+    this.oneSignal.startInit('b2f7f966-d8cc-11e4-bed1-df8f05be55ba', '703322744261');
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    this.oneSignal.handleNotificationReceived().subscribe((notification) => {
+      console.log('notification received: ', notification);
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe((notification) => {
+      console.log('notification opened: ', notification);
+    });
+
+    this.oneSignal.endInit();
+  }
 }
