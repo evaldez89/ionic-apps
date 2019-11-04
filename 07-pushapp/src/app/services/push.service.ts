fcde8e5c6ea42b0b52427e40f8871ba8214e2d8a
Emmanuel Valdez <evaldez89@gmail.com>
2019-11-03 23:14:37 -0400

    set project id from onesignal and project number from firebase

--- 07-pushapp/src/app/services/push.service.ts
+++ 07-pushapp/src/app/services/push.service.ts
@@ -9,7 +9,7 @@ export class PushService {
   constructor( private oneSignal: OneSignal ) { }
 
   initialConfig() {
-    this.oneSignal.startInit('b2f7f966-d8cc-11e4-bed1-df8f05be55ba', '703322744261');
+    this.oneSignal.startInit('6f1b8334-3762-47c2-b265-bef31daa8191', '277605051574');
 
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
 
