2fc0f3659e3a50538906d62f1c823e58d33352de
Emmanuel Valdez <evaldez89@gmail.com>
2020-02-29 23:41:31 -0400

    add mongo conection

--- 08-fotosgram-server/index.ts
+++ 08-fotosgram-server/index.ts
@@ -1,8 +1,17 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/user";
+import mongoose from 'mongoose';
 
 
 const server = new Server();
+
+mongoose.connect('mongodb://localhost:27017/fotosgram',
+                 { useNewUrlParser: true, useCreateIndex: true },
+                 (error) => {
+                     if (error) throw error;
+
+                     console.log('MONGO DB ONLINE');
+                 });
 
 server.app.use('/users', userRoutes);
 
