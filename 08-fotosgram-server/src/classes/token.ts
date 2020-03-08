f52e948b252933bf5b2d60bf8456329c9b1a633f
Emmanuel Valdez <evaldez89@gmail.com>
2020-03-08 19:07:02 -0400

    fix token verification resolver

--- 08-fotosgram-server/src/classes/token.ts
+++ 08-fotosgram-server/src/classes/token.ts
@@ -15,7 +15,7 @@ export default class Token {
                 if (error) {
                     reject();
                 } else {
-                    resolve();
+                    resolve( decode );
                 }
             });
         });
